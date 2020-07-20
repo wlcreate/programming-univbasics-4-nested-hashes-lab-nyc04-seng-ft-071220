03afafb66c430f03acc812a68bf9b99eece8b260
Waverley Leung <waverley.nyc@gmail.com>
2020-07-20 15:42:47 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,12 +15,11 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -36,7 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +54,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +80,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}, {:languages => ["LISP", "C"]}
+    programmer_hash
 end
 
 def changing_alan
@@ -103,7 +103,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +126,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
